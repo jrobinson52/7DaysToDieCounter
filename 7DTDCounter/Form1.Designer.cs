e8a17384598207f51fb820fb97cc9ccf7587fcac
jrobinson52 <31286997+jrobinson52@users.noreply.github.com>
2017-08-28 19:22:26 -0400

    Add project files.

---
+++ 7DTDCounter/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace _7DTDCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Day:";
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(122, 96);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(100, 22);
+            this.txtDay.TabIndex = 1;
+            this.txtDay.TextChanged += new System.EventHandler(this.txtDay_TextChanged);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(119, 139);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(65, 17);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Text = "lblOutput";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 250);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ 7DTDCounter/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7DTDCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDay_TextChanged(object sender, EventArgs e)
+        {
+            // catch if nothing is entered
+            if (txtDay.Text == "")
+                txtDay.Text = "0";
+
+            // check every character input in txtDay to ensure it is a digit
+            string var = "";
+            foreach (char c in txtDay.Text)
+                if (char.IsDigit(c))
+                    var = var + c;
+
+            int day = Convert.ToInt32(txtDay.Text); //define day and make input integer
+            int nextDay = ((day /7) +1) * 7; //define next day and calculate next day seven
+            int daysLeft = nextDay - day; //determine how many days are left till 7th day
+
+            lblOutput.Text = nextDay.ToString(); //output next 7th day
+
+
+        }
+    }
+}
